4ef7f6acfe43d36caf3b83c7f02264182ee5ed44
nithyakalathil <nithya913k@gmail.com>
2024-06-07 22:38:57 +0530

    Photoapp created

---
+++ src/components/Addphotos.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Addphotos = () => {
+  return (
+    <div>
+<Navbar/>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="row g-3">
+<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="" className="form-label">Title</label>
+<input type="text" className="form-control" />
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+    <label htmlFor="" className="form-label">Title id</label>
+    <input type="text" className="form-control" />
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+    <label htmlFor="" className="form-label">Photo</label>
+    <input type="file" name="" id="" className="form-control" />
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+    <label htmlFor="" className="form-label">Category</label>
+    <select name="" id="" className="form-control">
+        <option value="album">album</option>
+        <option value="actors">actors</option>
+    </select>
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+    <label htmlFor="" className="form-label">discription</label>
+    <textarea name="" id="" className="form-control"></textarea>
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<button className="btn btn-success">Submit</button>
+
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Addphotos
